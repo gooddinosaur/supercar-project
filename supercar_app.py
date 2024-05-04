b6546ac2d486ae85f443fee20dba73e7806586cc
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-05-04 17:43:13 +0700

    Updated supercar_app.py(Using supercar_model)

--- supercar_app.py
+++ supercar_app.py
@@ -1,10 +1,9 @@
 from supercar_controller import SupercarController
 from csv_reader import Read
-
+from supercar_model import  SupercarModel
 
 if __name__ == '__main__':
     data = Read('data.csv')
     data.insert()
-    Supercar = SupercarController(data.info)
+    Supercar = SupercarController(SupercarModel(data.info))
     Supercar.ui.run()
-
