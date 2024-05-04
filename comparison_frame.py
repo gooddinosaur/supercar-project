93e417f634683c58c76c59de737ee1c597ad291a
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-05-04 17:42:24 +0700

    Updated comparison_frame.py(Finished only UI)

--- comparison_frame.py
+++ comparison_frame.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
+
 class ComparisonFrame(tk.Frame):
     def __init__(self, parent, ui,controller, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
@@ -27,12 +28,33 @@ class ComparisonFrame(tk.Frame):
         # Comparison box
         compare_lable = tk.Label(self, text="Comparison box", font=("Helvetica", 12))
         compare_lable.pack(side=tk.TOP, anchor="w")
-        compare_box = tk.Listbox(self, font=('Arial', 12), width=30, height=15)
+        compare_box = tk.Listbox(self, font=('Arial', 12), width=40, height=10)
         compare_box.pack(side=tk.TOP)
+
+        # Button for interact with comparison box
+        interact_buttons = InteractButton(self, self.controller)
+        interact_buttons.pack(side=tk.TOP)
 
         # Back to Main Menu Button
         back_button = tk.Button(self, text="Back to Main Menu", command=self.ui.show_startup_window)
         back_button.pack(side=tk.BOTTOM)
 
 
+class InteractButton(tk.Frame):
+    def __init__(self, parent, controller, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.controller = controller
+        self.init_components()
 
+    def init_components(self) -> None:
+        configurations = {'side': tk.LEFT, 'padx': 5, 'pady': 10}
+
+        # Button for interact with comparison box
+        compare_button = tk.Button(self, text="Compare")
+        compare_button.pack(**configurations)
+
+        clear_button = tk.Button(self, text="Clear")
+        clear_button.pack(**configurations)
+
+        remove_button = tk.Button(self, text="Remove")
+        remove_button.pack(**configurations)
