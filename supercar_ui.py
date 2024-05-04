f0b9f8f62d6f484c3a0224cb7aa799154940e535
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-05-04 12:39:04 +0700

    Added real time result for users input in search box

--- supercar_ui.py
+++ supercar_ui.py
@@ -40,10 +40,10 @@ class SupercarUI(tk.Tk):
         startup_label.pack(**configurations)
 
         startup_button = tk.Button(self.main_frame, text="Start", width=10, height=2, command=self.show_main_window)
-        startup_button.pack(**configurations)
+        startup_button.pack(side="top", padx=20, pady=10)
 
         quit_button = tk.Button(self.main_frame, text="Quit", width=10, height=2, command=self.quit_program)
-        quit_button.pack(**configurations)
+        quit_button.pack(side="top", padx=20, pady=10)
 
     def show_main_window(self):
         self.main_frame.destroy()
@@ -51,14 +51,14 @@ class SupercarUI(tk.Tk):
         self.main_frame.pack(fill="both", expand=True)
 
         # Configurations
+        configurations = {'padx': 10, 'pady': 10}
 
         # Search Box
         search_box_label = tk.Label(self.main_frame, text="Search Box")
         search_box_label.grid(row=0, column=0)
         self.search_box = tk.Entry(self.main_frame, width=25, textvariable=self.search)
         self.search_box.grid(row=1, column=0)
-        search_button = tk.Button(self.main_frame, text="Search", command=self.controller.show_search_result)
-        search_button.grid(row=1, column=1)
+        self.search_box.bind("<KeyRelease>", self.on_search_key_release)
 
         # Result list box
         result_label = tk.Label(self.main_frame, text="Results")
@@ -84,6 +84,24 @@ class SupercarUI(tk.Tk):
         # Back to Main Menu Button
         main_button = tk.Button(self.main_frame, text="Back to Main Menu",
                                 command=self.show_startup_window)
+
+        analysis_buttons = {
+            "Descriptive Statistic": self.controller.show_descriptive_statistic,
+            "Distribution": self.controller.show_distribution,
+            "Correlation": self.controller.show_correlation,
+            "Part-to-Whole": self.controller.show_part_to_whole,
+            "Time-Series": self.controller.show_time_series
+        }
+        row_index = 3
+        for button_text, button_command in analysis_buttons.items():
+            button = tk.Button(self.main_frame, text=button_text,
+                               command=button_command)
+            button.grid(row=row_index, column=1200, columnspan=1200,
+                        **configurations)
+            row_index += 1
+
+    def on_search_key_release(self, event):
+        self.controller.show_search_result()
 
     def quit_program(self):
         self.destroy()
