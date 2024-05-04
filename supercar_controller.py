d78cc45ab96e2c00b70320afba0fd9ee0f01f12c
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-05-04 17:44:50 +0700

    Updated supercar ui and controller. To use with supercar model

--- supercar_controller.py
+++ supercar_controller.py
@@ -1,47 +1,44 @@
 from supercar_ui import SupercarUI
+from supercar_model import SupercarModel
 import tkinter as tk
-import tkinter.messagebox as messagebox
+import matplotlib.pyplot as plt
+import numpy as np
+from tkinter import ttk
+
 
 class SupercarController:
     """Controller class for the calculator application."""
 
-    def __init__(self, data):
+    def __init__(self, model):
         self.ui = SupercarUI(self)
-        self.data = data
+        self.model = model
 
     def show_search_result(self):
-        search_text = self.ui.search.get()
+        search_text = self.ui.search_results_frame.search.get()
         if not search_text:
-            messagebox.showerror("Error", "The search box is empty.")
-            self.ui.result_box.delete(0, tk.END)
+            self.ui.search_results_frame.result_box.delete(0, tk.END)
             return
-        result = []
-        for car in self.data:
-            if search_text.lower() in car['Car Make'].lower() + "-" + car['Car Model'].lower():
-                result.append(car)
-        self.ui.result_box.delete(0, tk.END)
+        result = self.model.get_search_result(search_text)
+        self.ui.search_results_frame.result_box.delete(0, tk.END)
         for car in result:
-            self.ui.result_box.insert(tk.END, f"{car['Car Make']} - {car['Car Model']}")
+            self.ui.search_results_frame.result_box.insert(tk.END, f"{car['Car Make']} - {car['Car Model']}")
 
     def on_car_select(self, event):
         # Get the selected car from the result box
-        selected_index = self.ui.result_box.curselection()
+        selected_index = self.ui.search_results_frame.result_box.curselection()
         if selected_index:
             index = int(selected_index[0])
-            self.selected_car = self.data[index]
-
-            # Enable the "Show specs" button and "Add to compare list" button
-            self.ui.show_spec_button['state'] = tk.NORMAL
-            self.ui.add_com_button['state'] = tk.NORMAL
+            self.selected_car = self.model.data[index]
+            self.ui.search_results_frame.show_spec_button['state'] = tk.NORMAL
+            self.ui.search_results_frame.add_com_button['state'] = tk.NORMAL
 
     def show_car_specs(self):
         # Get the selected car from the result box
-        selected_index = self.ui.result_box.curselection()
+        selected_index = self.ui.search_results_frame.result_box.curselection()
         if selected_index:
             index = int(selected_index[0])
-            selected_car = self.ui.result_box.get(index)
-            print(selected_car)
-            selected_car_info = self.car_getter(selected_car)
+            selected_car = self.ui.search_results_frame.result_box.get(index)
+            selected_car_info = self.model.car_getter(selected_car)
             # Create a new window to display the information about the selected car
             car_specs_window = tk.Toplevel()
             car_specs_window.title("Car Specs")
@@ -52,8 +49,63 @@ class SupercarController:
                 label = tk.Label(car_specs_frame, text=f"{key}: {value}")
                 label.pack()
 
-    def car_getter(self, text):
-        for car in self.data:
-            if car['Car Make'] + " - " + car['Car Model'] == text:
-                return car
+    def show_distribution(self):
+        # Clear previous content
+        for widget in self.ui.main_frame.winfo_children():
+            widget.destroy()
 
+        # Configurations
+        configurations = {'padx': 10, 'pady': 10}
+
+        # Add combobox to select attribute
+        ttk.Label(self.ui.main_frame, text="Select Attribute:").grid(row=0,
+                                                                     column=0,
+                                                                     **configurations)
+        attribute_combo = ttk.Combobox(self.ui.main_frame,
+                                       values=['Year', 'Engine Size (L)',
+                                               'Horsepower', 'Torque (lb-ft)',
+                                               '0-60 MPH Time (seconds)',
+                                               'Price (in USD)'])
+        attribute_combo.grid(row=0, column=1, **configurations)
+
+        # Add button to generate distribution graph
+        ttk.Button(self.ui.main_frame, text="Generate",
+                   command=lambda: self.generate_distribution(
+                       attribute_combo.get())).grid(row=1, column=0,
+                                                    columnspan=2,
+                                                    **configurations)
+        ttk.Button(self.ui.main_frame, text="Back",
+                   command=self.ui.show_main_window).grid(row=2, column=0,
+                                                          columnspan=2,
+                                                          **configurations)
+
+    def generate_descriptive_statistic(self, attribute):
+        pass
+
+    def generate_distribution(self, attribute):
+        data = []
+        for car in self.model.data:
+            try:
+                if attribute == 'Price (in USD)':
+                    value = eval(car[attribute].replace(',', ''))
+                else:
+                    value = float(car[attribute])
+                data.append(value)
+            except (ValueError, TypeError):
+                pass
+
+        # Create a histogram
+        plt.hist(data)
+        plt.title(f'Distribution - Histogram of {attribute}')
+        plt.xlabel(attribute)
+        plt.ylabel('Frequency')
+        plt.show()
+
+    def generate_correlation(self, attribute):
+        pass
+
+    def generate_part_to_whole(self, attribute):
+        pass
+
+    def generate_time_series(self, attribute):
+        pass
--- supercar_ui.py
+++ supercar_ui.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 from search_result_frame import SearchResultsFrame
+from comparison_frame import ComparisonFrame
+from statistic_frame import StatisticFrame
+
 
 class SupercarUI(tk.Tk):
     def __init__(self, controller):
@@ -14,7 +17,7 @@ class SupercarUI(tk.Tk):
     def init_main_window(self):
         self.main_frame = tk.Frame(self)
         self.main_frame.pack(fill="both", expand=True)
-        self.minsize(1200, 500)
+        # self.minsize(1200, 500)
         self.show_startup_window()
 
     def show_startup_window(self):
@@ -22,14 +25,13 @@ class SupercarUI(tk.Tk):
         self.main_frame = tk.Frame(self)
         self.main_frame.pack(fill="both", expand=True)
 
-
         # New frame for showing pictures
         picture_frame = tk.Frame(self.main_frame, width=200)
         picture_frame.pack(side="left", fill="y")
 
         # Import and display picture
         image = Image.open("Images/startup_image.jpg")
-        image = image.resize((850, 500), Image.LANCZOS)
+        image = image.resize((772, 454), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
 
         picture_label = tk.Label(picture_frame, image=photo)
@@ -53,77 +55,18 @@ class SupercarUI(tk.Tk):
 
         # Left side frame
         self.search_results_frame = SearchResultsFrame(self.main_frame, self.controller)
-        self.search_results_frame.grid(row=0, column=0, padx=10, pady=10)
+        self.search_results_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
-        # Welcome Label
-        main_label = tk.Label(self.main_frame, text="Supercar choosing helper and analysis", font=("Helvetica", 16))
-        main_label.grid(row=0, column=1)
+        # Middle frame
+        self.comparison_frame = ComparisonFrame(self.main_frame, self, self.controller)
+        self.comparison_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
-
-        # Back to Main Menu Button
-        main_button = tk.Button(self.main_frame, text="Back to Main Menu", command=self.show_startup_window)
-
-        # Analysis button
-        analysis_buttons = {
-            "Descriptive Statistic": self.show_statistic_window,
-            "Distribution": self.show_distribution_window,
-            "Correlation": self.show_correlation_window,
-            "Part-to-Whole": self.show_part_to_whole_window,
-            "Time-Series": self.show_time_series__window
-        }
-        row_index = 0
-        for button_text, button_command in analysis_buttons.items():
-            button = tk.Button(self.main_frame, text=button_text, command=button_command)
-            button.grid(row=row_index, column=3, sticky='e')
-            row_index += 1
-
-        # Decoration pictures
-
-        # picture_frame = tk.Frame(self.main_frame, width=200)
-        # picture_frame.grid(row=1, column=2, padx=10, pady=10)
-        # image = Image.open("Images/descriptive statistic.png")
-        # # image = image.resize((850, 500), Image.LANCZOS)
-        # photo = ImageTk.PhotoImage(image)
-        #
-        # picture_label = tk.Label(picture_frame, image=photo)
-        # picture_label.image = photo
-        # picture_label.pack(pady=10)
-
-    def on_search_key_release(self, event):
-        self.controller.show_search_result()
+        # Right frame
+        self.statistic_frame = StatisticFrame(self.main_frame, self.controller)
+        self.statistic_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
     def quit_program(self):
         self.destroy()
 
     def run(self):
         self.mainloop()
-
-    def show_statistic_window(self):
-        pass
-
-    def show_distribution_window(self):
-        # Clear previous content
-        for widget in self.main_frame.winfo_children():
-            widget.destroy()
-
-        # Configurations
-        configurations = {'padx': 10, 'pady': 10}
-
-        # Add combobox to select attribute
-        ttk.Label(self.main_frame, text="Select Attribute:").grid(row=0, column=0, **configurations)
-        attribute_combo = ttk.Combobox(self.main_frame, values=['Year', 'Engine Size (L)', 'Horsepower', 'Torque (lb-ft)', '0-60 MPH Time (seconds)', 'Price (in USD)'])
-        attribute_combo.grid(row=0, column=1, **configurations)
-
-        # Add button to generate distribution graph
-        ttk.Button(self.main_frame, text="Generate", command=lambda: self.controller.generate_distribution(attribute_combo.get())).grid(row=1, column=0, columnspan=2, **configurations)
-        ttk.Button(self.main_frame, text="Back", command=self.show_main_window).grid(row=2, column=0, columnspan=2, **configurations)
-
-    def show_correlation_window(self):
-        pass
-
-    def show_part_to_whole_window(self):
-        pass
-
-    def show_time_series__window(self):
-        pass
-
