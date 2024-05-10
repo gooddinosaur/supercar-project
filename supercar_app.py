6b663a3246f0fd8a8ecb2991bb56e3305092e6e8
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-05-10 21:31:14 +0700

    Added docstring in suparcar_app.py

--- supercar_app.py
+++ supercar_app.py
@@ -1,6 +1,12 @@
+"""Main script for running the supercar application.
+
+This script initializes the necessary components for running the supercar application,
+including reading data from a CSV file, initializing the model with the read data,
+creating a controller with the initialized model, and running the user interface."""
+
 from supercar_controller import SupercarController
 from csv_reader import Read
-from supercar_model import  SupercarModel
+from supercar_model import SupercarModel
 
 if __name__ == '__main__':
     data = Read('data.csv')
